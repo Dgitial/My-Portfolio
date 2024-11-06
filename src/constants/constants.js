3651b9cdba2d88ffeaa55af87764b716193a5395
Divine Basusi <divinebasusi2002@gmail.com>
2024-11-06 10:12:31 +0100

    Fix: Add @babel/core depepndency and constants update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Responsive react website with CSS",
     description:
       "A fully responsive website built with React for dynamic, interactive user experiences and styled with CSS for a clean, adaptive design that looks great on any device.",
-    image: "/My-Portfolio/images/1.png",
+    image: "/images/1.png",
     tags: ["React", "Node"],
     source: "https://github.com/Dgitial/Gpt3_jsm.git",
     visit: "https://gpt3-jsm-q549.vercel.app/",
@@ -13,7 +13,7 @@ export const projects = [
     title: "Crypto_Tracker",
     description:
       "responsive app built with React, providing live tracking and updates on cryptocurrency prices through API integration. Perfect for real-time insights and monitoring trends in the digital currency market.",
-    image: "/My-Portfolio/images/2.png",
+    image: "/images/2.png",
     tags: ["React", "JavaScript", "API-integration"],
     source: "https://github.com/Dgitial/Crypto-Tracker.git",
     visit: "https://crypto-tracker-sigma-six.vercel.app/",
@@ -23,7 +23,7 @@ export const projects = [
     title: "A prototype SocialMedia App",
     description:
       "A foundational social media prototype built using PHP and MySQL, showcasing essential features like user authentication, profile management, and data storage. This app serves as a template for creating scalable social platforms with robust backend capabilities.",
-    image: "/My-Portfolio/images/3.png",
+    image: "/images/3.png",
     tags: ["PHP", "MySql"],
     source: "https://google.com",
     visit: "https://google.com",
@@ -33,7 +33,7 @@ export const projects = [
     title: "Chat_app",
     description:
       "A real-time chat application developed with React, Zustand for state management, and Firebase for seamless authentication and data storage. This app allows users to send and receive messages and images instantly in a responsive and user-friendly interface.",
-    image: "/My-Portfolio/images/4.png",
+    image: "/images/4.png",
     tags: ["React", "Zustand", "Firebase"],
     source: "https://github.com/Dgitial/Realtime_Chat_app.git",
     visit: "https://google.com",
