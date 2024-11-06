ee28a39fd2726583aadb3f3cf18025595d4db17d
Divine Basusi <divinebasusi2002@gmail.com>
2024-11-06 12:19:03 +0100

    next config test fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   basePath: "/My-Portfolio",
   assetPrefix: "/My-Portfolio", // Optional but recommended for static assets
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/My-Portfolio", // Ensure people are redirected to the base path
+        permanent: true,
+      },
+    ];
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.js$/,
