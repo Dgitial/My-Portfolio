bacbdfd6360809f2dd89720164aa7757b485290e
Divine Basusi <divinebasusi2002@gmail.com>
2024-11-06 12:26:07 +0100

    new fix fo next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  basePath: "/My-Portfolio",
-  assetPrefix: "/My-Portfolio", // Optional but recommended for static assets
   redirects() {
     return [
       {
         source: "/",
-        destination: "/My-Portfolio", // Ensure people are redirected to the base path
+        destination: "/My-Portfolio",
         permanent: true,
       },
     ];
