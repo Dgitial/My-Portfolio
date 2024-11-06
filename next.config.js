165f47d74a7a2f6b2c74da59b0b65f4d75363055
Divine Basusi <divinebasusi2002@gmail.com>
2024-11-06 10:38:56 +0100

    Fix: babelrc nextconfig and updated package.json

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
       use: {
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env"],
+          presets: ["next/babel"],
         },
       },
     });
